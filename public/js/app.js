5fb7f874592f5533dda137ddd922d6a552c2a93c
Nick Osman <Nick.Osman@softwire.com>
2022-08-25 15:04:01 +0100

    working hamburger menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  const randomiseButton = document.getElementById("randomise");
-  randomiseButton.addEventListener("click", ()=>{
+  // const randomiseButton = document.getElementById("randomise");
+  // randomiseButton.addEventListener("click", ()=>{
 
-    let posts = document.getElementsByClassName("post");
-    for(let i=0; i<posts.length; i++){
+  //   let posts = document.getElementsByClassName("post");
+  //   for(let i=0; i<posts.length; i++){
 
-      posts[i].style.background = `rgb(
-        ${Math.floor(Math.random()*255)}, 
-        ${Math.floor(Math.random()*255)}, 
-        ${Math.floor(Math.random()*255)}
-        )`;
-    }
-  });
+  //     posts[i].style.background = `rgb(
+  //       ${Math.floor(Math.random()*255)}, 
+  //       ${Math.floor(Math.random()*255)}, 
+  //       ${Math.floor(Math.random()*255)}
+  //       )`;
+  //   }
+  // });
 
   const hamburgerButton = document.getElementById("hamburger-button");
+  const hamburgerDropdownLinks = document.getElementsByClassName("hamburger-dropdown");
   hamburgerButton.addEventListener("click", ()=>{
-    //change display property from :none to :block
-    //of dropdown-content class
+
+    if (hamburgerDropdownLinks[0].className === "hamburger-dropdown open"){
+      hamburgerDropdownLinks[0].className = "hamburger-dropdown closed"
+    }
+    else if (hamburgerDropdownLinks[0].className === "hamburger-dropdown closed"){
+      hamburgerDropdownLinks[0].className = "hamburger-dropdown open"
+    }
+
+
     ;
   });
 
