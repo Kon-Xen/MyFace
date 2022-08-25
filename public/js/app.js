7c25ba7cd50c43e58945b76698eef1516c2b48e1
Nick Osman <Nick.Osman@softwire.com>
2022-08-25 15:14:27 +0100

    fixed randomiser bug

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  // const randomiseButton = document.getElementById("randomise");
-  // randomiseButton.addEventListener("click", ()=>{
+  if (document.getElementById("randomise")){
+    const randomiseButton = document.getElementById("randomise");
+    randomiseButton.addEventListener("click", ()=>{
+  
+      let posts = document.getElementsByClassName("post");
+      for(let i=0; i<posts.length; i++){
+  
+        posts[i].style.background = `rgb(
+          ${Math.floor(Math.random()*255)}, 
+          ${Math.floor(Math.random()*255)}, 
+          ${Math.floor(Math.random()*255)}
+          )`;
+      }
+    });
+  }
 
-  //   let posts = document.getElementsByClassName("post");
-  //   for(let i=0; i<posts.length; i++){
-
-  //     posts[i].style.background = `rgb(
-  //       ${Math.floor(Math.random()*255)}, 
-  //       ${Math.floor(Math.random()*255)}, 
-  //       ${Math.floor(Math.random()*255)}
-  //       )`;
-  //   }
-  // });
 
   const hamburgerButton = document.getElementById("hamburger-button");
   const hamburgerDropdownLinks = document.getElementsByClassName("hamburger-dropdown");
